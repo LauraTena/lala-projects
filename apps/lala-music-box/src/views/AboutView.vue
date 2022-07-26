f38b8c438b7b34d74d33e672fc80e8c3aeb2f37e
Laura Tena <laura.tena@frontiersin.org>
2022-07-26 18:51:10 +0200

    authorization fetch working

--- apps/lala-music-box/src/views/AboutView.vue
+++ apps/lala-music-box/src/views/AboutView.vue
@@ -2,6 +2,54 @@
   <h3>About</h3>
 </template>
 
-<script></script>
+<script lang="js">
+import { defineComponent } from "vue";
+import { Buffer } from "buffer";
 
+const clientId = import.meta.env.VITE_APP_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_APP_CLIENT_SECRET;
+
+export default defineComponent({
+  mounted() {
+    const requestPost = new Request('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+      },
+      body: 'grant_type=client_credentials',
+    });
+
+    const requestGet = (accessToken) => new Request(`https://api.spotify.com/v1/playlists/7vZzgb5MvHRW1nyUU5P3js/tracks`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    fetch(requestPost)
+      .then(async (response) => {
+        const res = await response.json();
+        const accessToken = res.access_token;
+
+        fetch(requestGet(accessToken))
+          .then(async (responseGet) => {
+            await responseGet.json()
+              .then((data) => {
+                console.log(data.items)
+              });
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+});
+</script>
 <style></style>
