fc7a0537f29a941df35b66c5ca785ad740d08aa0
Laura Tena <laura.tena@frontiersin.org>
2022-08-15 18:59:15 +0200

    AwwardsCoursePageTransiton_Vertex Shader

--- packages/animations/src/core/pages/01/shaders/vertex.glsl
+++ packages/animations/src/core/pages/01/shaders/vertex.glsl
@@ -1,3 +1,9 @@
+uniform float time;
+varying float pulse;
+
 void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
+    vec3 newPosition = position;
+    newPosition.z = sin(length(position) * 30. + time) * 0.1;
+    pulse = 2. * newPosition.z;
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( newPosition, 1.0 );
 }
